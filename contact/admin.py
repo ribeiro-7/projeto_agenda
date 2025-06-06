329405d5ae381bd3d56b5d3e84ccb02224ae4ff4
ribeirin-7 <arthurr366@gmail.com>
2025-06-06 13:32:40 -0300

    Cadastrando Admin de Contact

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from contact import models
 
-# Register your models here.
+@admin.register(models.Contact)
+class ContactAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'phone']
+    ordering = ['first_name']
+    list_filter = 'created_data',
