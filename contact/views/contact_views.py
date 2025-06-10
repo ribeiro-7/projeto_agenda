751ba380de2c5d91f273a2109c6946bb1265f03e
ribeirin-7 <arthurr366@gmail.com>
2025-06-10 11:24:50 -0300

    adicionando site_title nas views para head

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -6,7 +6,8 @@ def index(request):
     contacts = Contact.objects.filter(show=True).order_by('-id')
 
     context = {
-        'contacts': contacts
+        'contacts': contacts,
+        'site_title': 'Contatos - '
     } 
 
     return render(
@@ -20,7 +21,8 @@ def contact(request, contact_id):
     single_contact = get_object_or_404(Contact, pk=contact_id, show=True)
 
     context = {
-        'contact': single_contact
+        'contact': single_contact,
+        'site_title': f'{single_contact.first_name} {single_contact.last_name} - '
     }
 
     return render(
