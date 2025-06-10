e7f22caa83f9678dad57e8a5c1d5348a4010979b
ribeirin-7 <arthurr366@gmail.com>
2025-06-10 16:19:08 -0300

    Implementando o formulario de contato

---
+++ contact/forms.py
@@ -0,0 +1,35 @@
+from django import forms
+from contact.models import Contact
+
+class ContactForm(forms.ModelForm):
+
+    class Meta:
+        model = Contact
+        fields = ('first_name', 'last_name', 'phone', 'email', 'description', 'category')
+        widgets = {
+            'first_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter the first name. (Obrigatório)'
+                }
+            ),
+            'last_name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter the last name.'
+                }
+            ),
+            'phone': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter the phone number. format: (**) *****-****. (Obrigatório)'
+                },
+            ),
+            'email': forms.TextInput(
+                attrs={
+                    'placeholder': 'Enter the email.'
+                }
+            ),
+            'description': forms.Textarea(
+                attrs={
+                    'placeholder': 'Enter the description.'
+                }
+            )
+        }
