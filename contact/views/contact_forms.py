37e48fd35665e48876060283b7e6aa72fe826d4b
ribeirin-7 <arthurr366@gmail.com>
2025-06-10 17:04:29 -0300

    Implementando a view de update e form_action para redirecionamento entre create e update

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -1,22 +1,26 @@
 from contact.forms import ContactForm
+from contact.models import Contact
 
-from django.core.paginator import Paginator
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect, get_object_or_404
+from django.urls import reverse
 
 
 def create(request):
+
+    form_action = reverse('contact:create')
 
     if request.method == 'POST':
 
         form = ContactForm(request.POST)
 
         context = {
-            'form': form
+            'form': form,
+            'form_action': form_action
         }
 
         if form.is_valid():
-            form.save()
-            return redirect('contact:create')
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
 
 
         return render(
@@ -26,7 +30,44 @@ def create(request):
         )
     
     context = {
-        'form': ContactForm()
+        'form': ContactForm(),
+        'form_action': form_action
+    }
+
+    return render(
+            request,
+            'contact/create.html',
+            context
+    )
+
+def update(request, contact_id):
+
+    contact = get_object_or_404(Contact, pk=contact_id, show=True)
+
+    form_action = reverse('contact:update', kwargs={'contact_id': contact_id})
+
+    if request.method == 'POST':
+
+        form = ContactForm(request.POST, instance=contact)
+
+        context = {
+            'form': form,
+            'form_action': form_action
+        }
+
+        if form.is_valid():
+            contact = form.save()
+            return redirect('contact:update', contact_id=contact.pk)
+        
+        return render(
+            request,
+            'contact/create.html',
+            context
+        )
+    
+    context = {
+        'form': ContactForm(instance=contact),
+        'form_action': form_action
     }
 
     return render(
