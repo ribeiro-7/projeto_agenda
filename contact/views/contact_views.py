00175e61cc54ff649f585742b9c26b3e9754a6c3
ribeirin-7 <arthurr366@gmail.com>
2025-06-10 12:22:33 -0300

    Implementando view de search

--- contact/views/contact_views.py
+++ contact/views/contact_views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from contact.models import Contact
+from django.db.models import Q
 
 def index(request):
 
@@ -8,6 +9,35 @@ def index(request):
     context = {
         'contacts': contacts,
         'site_title': 'Contatos - '
+    } 
+
+    return render(
+        request,
+        'contact/index.html',
+        context
+    )
+
+def search(request):
+
+    search_value = request.GET.get('q', '').strip()
+
+    if search_value == '':
+        return redirect('contact:index')
+
+    contacts = Contact.objects \
+        .filter(show=True) \
+        .filter(
+            Q(first_name__icontains=search_value) | 
+            Q(last_name__icontains=search_value) | 
+            Q(phone__icontains=search_value) | 
+            Q(email__icontains=search_value)
+        ) \
+        .order_by('-id')
+
+    context = {
+        'contacts': contacts,
+        'site_title': 'Contatos - ',
+        'search_value': search_value
     } 
 
     return render(
