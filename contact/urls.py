67b89546ee03d4808553ed389941691fcb893dae
ribeirin-7 <arthurr366@gmail.com>
2025-06-11 16:46:47 -0300

    Adicionando url de update de user

--- contact/urls.py
+++ contact/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('user/register/', views.register, name='register'),
     path('user/login/', views.login, name='login'),
     path('user/logout/', views.logout, name='logout'),
+    path('user/update/', views.user_update, name='user_update'),
 
     #agenda
     path('search/', views.search, name='search'),
