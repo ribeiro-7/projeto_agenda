014dbe065a16088e382d818ec07461c742ffc0a2
ribeirin-7 <arthurr366@gmail.com>
2025-06-11 14:09:20 -0300

    Adicionando o url de register user

--- contact/urls.py
+++ contact/urls.py
@@ -4,10 +4,16 @@ from contact import views
 app_name = 'contact'
 
 urlpatterns = [
+    #contact
     path('contact/<int:contact_id>/', views.contact, name='contact'),
     path('contact/<int:contact_id>/update/', views.update, name='update'),
     path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
     path('contact/create/', views.create, name='create'),
+
+    #user
+    path('user/register/', views.register, name='register'),
+    
+    #agenda
     path('search/', views.search, name='search'),
     path('', views.index, name='index'),
 ]
